1dae2f91578a1ef1549da6bedfd01279960f1b84
Trieu Tien Nguyen <nguyenthanhtung111xxx@gmail.com>
2024-01-12 09:30:57 +0700

    update something

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,23 +1,46 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class PostsService {
   constructor(private prismaService: PrismaService) { }
-  async create(createPostDto: CreatePostDto) {
-    try {
-      return await this.prismaService.post.create({ data: createPostDto })
-    } catch (error) {
+  async create(createPostDto: any) {
 
+
+    try {
+      const images = createPostDto?.images
+      delete createPostDto?.images
+      const post = await this.prismaService.post.create({ data: createPostDto })
+      if (post) {
+        const account = await this.prismaService.account.findFirst({ where: { id: post.accountId } })
+        await this.prismaService.image.createMany({
+          data: images?.map(image => {
+            return {
+              url: image.data_url,
+              postId: post?.id
+            }
+          })
+        })
+        return {
+          ...post,
+          account,
+          images: images.map(image => {
+            return {
+              url: image?.data_url
+            }
+          })
+        }
+      }
+
+    } catch (error) {
       throw new BadRequestException(error)
     }
   }
 
   async findAll() {
     try {
-      return await this.prismaService.post.findMany({})
+      return await this.prismaService.post.findMany({ include: { account: true, images: true }, orderBy: { id: 'desc' } })
     } catch (error) {
 
       throw new BadRequestException(error)
@@ -26,17 +49,49 @@ export class PostsService {
 
   async findOne(id: number) {
     try {
-      return await this.prismaService.post.findFirst({ where: { id } })
+      return await this.prismaService.post.findFirst({
+        where: { id }, include: {
+          account: true, images: true
+        }
+      })
     } catch (error) {
 
       throw new BadRequestException(error)
     }
   }
 
-  async update(id: number, updatePostDto: UpdatePostDto) {
+  async update(id: number, updatePostDto: any) {
     try {
-      return await this.prismaService.post.update({ where: { id }, data: updatePostDto })
+      const imagesList = updatePostDto.images;
+      delete updatePostDto.images;
+      const post = await this.prismaService.post.update({
+        data: updatePostDto,
+        where: { id },
+        include: {
+          account: true
+        }
+      });
+      await this.prismaService.image.deleteMany({
+        where: { postId: post.id },
+      });
+      await this.prismaService.image.createMany({
+        data: imagesList.map((image) => {
+          return {
+            postId: post.id,
+            url: image.url,
+          };
+        }),
+      });
+      const images = await this.prismaService.image.findMany({
+        where: { postId: post.id },
+      });
+
+      return {
+        images,
+        ...post,
+      };
     } catch (error) {
+      console.log(error);
 
       throw new BadRequestException(error)
     }
